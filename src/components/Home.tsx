048005b468151d04cc701aec805316c80d6161ea
Tak-Iwamoto <wolfgangwtwt@gmail.com>
2020-05-29 00:01:51 +0900

    fix-url-params

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,25 +1,13 @@
 import React, { FC } from 'react';
-import { Button, Grid } from 'semantic-ui-react';
-
-const ids: string[] = ['adKN6i_etg0', '2yPtcvVZs58', 'gP6sOIzFyYc'];
+import { Grid } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
 
 const Home: FC = () => {
   return (
     <Grid columns={3} centered>
       <Grid.Row>
         <Grid.Column>
-          <Button content="Primary" primary />
-        </Grid.Column>
-      </Grid.Row>
-      <Grid.Row>
-        <Grid.Column>
-          <Button content="Primary" primary />
-        </Grid.Column>
-        <Grid.Column>
-          <Button content="Primary" primary />
-        </Grid.Column>
-        <Grid.Column>
-          <Button content="Primary" primary />
+          <Link to="/videos/adKN6i_etg0">Video Test</Link>
         </Grid.Column>
       </Grid.Row>
     </Grid>
--- src/containers/Video.tsx
+++ src/containers/Video.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useEffect } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators, Dispatch } from 'redux';
-import { RouteComponentProps, withRouter } from 'react-router';
+import { Dispatch } from 'redux';
+import { useParams } from 'react-router';
 import { VideoType } from '../services/youtube/models';
 import Video, { VideoProps } from '../components/Video';
 import { VideoState } from '../reducers/videoReducer';
@@ -17,42 +17,30 @@ interface DispatchProps {
   getVideoStart: (videoId: string) => void;
 }
 
-type EnhancedVideoProps = VideoProps &
-  StateProps &
-  DispatchProps &
-  RouteComponentProps<{ videoId: string }>;
+type EnhancedVideoProps = VideoProps & StateProps & DispatchProps;
 
 const mapStateToProps = (state: VideoState): StateProps => ({
   video: state.video,
   isLoading: state.isLoading,
 });
 
-const mapDispatchToProps = (dispatch: Dispatch): DispatchProps =>
-  bindActionCreators(
-    {
-      getVideoStart: (videoId: string) => getVideo.start({ videoId }),
-    },
-    dispatch,
-  );
+const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => ({
+  getVideoStart: videoId => dispatch(getVideo.start({ videoId })),
+});
 
 const VideoContainer: FC<EnhancedVideoProps> = ({
   video,
   isLoading,
   getVideoStart,
-  match,
 }) => {
-  const { videoId } = match.params;
+  const { videoId } = useParams();
 
   useEffect(() => {
     getVideoStart(videoId);
   }, []);
 
-  console.log(video);
-
   if (isLoading) return <Spinner />;
   return <Video video={video} />;
 };
 
-export default withRouter(
-  connect(mapStateToProps, mapDispatchToProps)(VideoContainer),
-);
+export default connect(mapStateToProps, mapDispatchToProps)(VideoContainer);
